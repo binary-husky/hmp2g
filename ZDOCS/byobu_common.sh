eb0736b3f7d1c85dc794d08db621de8f6c2f7e35
hmp <505030475@qq.com>
2023-02-16 23:06:36 +0800

    up

--- ZDOCS/byobu_common.sh
+++ ZDOCS/byobu_common.sh
@@ -8,3 +8,27 @@ function bt1(){
     echo $SESSION_NAME;
     byobu attach -t $SESSION_NAME;
 }
+function btlast(){ 
+    SESSION_NAME=$(byobu ls | tail -n 1);
+    IFS=':' read -ra arr <<< $SESSION_NAME;
+    SESSION_NAME=${arr[0]};
+    echo $SESSION_NAME;
+    byobu attach -t $SESSION_NAME;
+}
+
+
+function bk1(){ 
+    SESSION_NAME=$(byobu ls | head -n 1);
+    IFS=':' read -ra arr <<< $SESSION_NAME;
+    SESSION_NAME=${arr[0]};
+    echo $SESSION_NAME;
+    byobu kill-session -t $SESSION_NAME;
+}
+
+function bklast(){ 
+    SESSION_NAME=$(byobu ls | tail -n 1);
+    IFS=':' read -ra arr <<< $SESSION_NAME;
+    SESSION_NAME=${arr[0]};
+    echo $SESSION_NAME;
+    byobu kill-session -t $SESSION_NAME;
+}
